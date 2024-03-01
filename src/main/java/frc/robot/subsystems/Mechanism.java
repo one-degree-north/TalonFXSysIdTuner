6dcf0bf0246113fd439f2d6295902429c2dfe369
marcmchin <32217062+marcmchin@users.noreply.github.com>
2024-03-01 13:45:26 +0800

    Init

--- src/main/java/frc/robot/subsystems/Mechanism.java
+++ src/main/java/frc/robot/subsystems/Mechanism.java
@@ -4,14 +4,9 @@ import static edu.wpi.first.units.Units.Volts;
 
 import java.util.function.DoubleSupplier;
 
-import com.ctre.phoenix6.BaseStatusSignal;
 import com.ctre.phoenix6.SignalLogger;
-import com.ctre.phoenix6.configs.TalonFXConfiguration;
-import com.ctre.phoenix6.controls.DutyCycleOut;
-import com.ctre.phoenix6.controls.Follower;
-import com.ctre.phoenix6.controls.VoltageOut;
-import com.ctre.phoenix6.hardware.TalonFX;
-import com.ctre.phoenix6.signals.InvertedValue;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Voltage;
@@ -21,13 +16,12 @@ import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class Mechanism extends SubsystemBase {
     /* TODO: Set CAN ID and CAN Bus */
-    private final TalonFX m_motorToTest = new TalonFX(0, "rio");
+    private final CANSparkMax m_motorToTest = new CANSparkMax(0, MotorType.kBrushless);
 
     /* TODO: Uncomment this line to add a follower motor */
-    private final TalonFX m_followerMotorToTest = new TalonFX(1, "rio");
+    private final CANSparkMax m_followerMotorToTest = new CANSparkMax(0, MotorType.kBrushless);
     
-    private final DutyCycleOut m_joystickControl = new DutyCycleOut(0);
-    private final VoltageOut m_sysidControl = new VoltageOut(0);
+    
 
     private SysIdRoutine m_SysIdRoutine =
         new SysIdRoutine(
@@ -38,7 +32,7 @@ public class Mechanism extends SubsystemBase {
                                        // Log state with Phoenix SignalLogger class
                 (state)->SignalLogger.writeString("state", state.toString())),
             new SysIdRoutine.Mechanism(
-                (Measure<Voltage> volts)-> m_motorToTest.setControl(m_sysidControl.withOutput(volts.in(Volts))),
+                (Measure<Voltage> volts)-> m_motorToTest.setVoltage(volts.in(Volts)),
                 null,
                 this));
 
@@ -47,39 +41,23 @@ public class Mechanism extends SubsystemBase {
 
         configureMotors();
 
-        /* Speed up signals for better charaterization data */
-        BaseStatusSignal.setUpdateFrequencyForAll(250,
-            m_motorToTest.getPosition(),
-            m_motorToTest.getVelocity(),
-            m_motorToTest.getMotorVoltage());
-
-        /* Optimize out the other signals, since they're not particularly helpful for us */
-        m_motorToTest.optimizeBusUtilization();
-
         SignalLogger.start();
     }
 
     public void configureMotors() {
-        /* TODO: Edit this method to add follower motor and set
-           SensorToMechanismRatio, MotorInvert
-         */
-        TalonFXConfiguration cfg = new TalonFXConfiguration();
-        cfg.CurrentLimits.SupplyCurrentLimitEnable = true;
-        cfg.CurrentLimits.SupplyCurrentLimit = 35;
-        cfg.CurrentLimits.SupplyCurrentThreshold = 60;
-        cfg.CurrentLimits.SupplyTimeThreshold = 0.1;
+        m_motorToTest.restoreFactoryDefaults();
+        m_motorToTest.setSmartCurrentLimit(60);
+        // GEAR RATIO (convert from motor rotations to output rotations)
+        m_motorToTest.getEncoder().setPositionConversionFactor(1);
 
-        cfg.Feedback.SensorToMechanismRatio = 1;
-        cfg.MotorOutput.Inverted = InvertedValue.CounterClockwise_Positive;
+        // GEAR RATIO DIVIDED BY 60 (convert from motor RPM to output RPM)
+        m_motorToTest.getEncoder().setVelocityConversionFactor(1/60);
 
-        m_motorToTest.getConfigurator().apply(cfg);
-
-        /* TODO: Uncomment to add follower motor */
-        m_followerMotorToTest.setControl(new Follower(m_motorToTest.getDeviceID(), true));
+        m_followerMotorToTest.follow(m_followerMotorToTest);
     }
 
     public Command joystickDriveCommand(DoubleSupplier output) {
-        return run(()->m_motorToTest.setControl(m_joystickControl.withOutput(output.getAsDouble())));
+        return run(()->m_motorToTest.set(output.getAsDouble()));
     }
     public Command sysIdQuasistatic(SysIdRoutine.Direction direction) {
         return m_SysIdRoutine.quasistatic(direction);
