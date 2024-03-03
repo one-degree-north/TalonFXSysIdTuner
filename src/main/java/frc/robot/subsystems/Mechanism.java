177894a98cbf3dfb5c28eaed6e8b571842c137f8
marcmchin <32217062+marcmchin@users.noreply.github.com>
2024-03-03 17:41:25 +0800

    Fixes

--- src/main/java/frc/robot/subsystems/Mechanism.java
+++ src/main/java/frc/robot/subsystems/Mechanism.java
@@ -10,16 +10,17 @@ import com.revrobotics.CANSparkLowLevel.MotorType;
 
 import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Voltage;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
 
 public class Mechanism extends SubsystemBase {
     /* TODO: Set CAN ID and CAN Bus */
-    private final CANSparkMax m_motorToTest = new CANSparkMax(0, MotorType.kBrushless);
+    private final CANSparkMax m_motorToTest = new CANSparkMax(16, MotorType.kBrushless);
 
     /* TODO: Uncomment this line to add a follower motor */
-    private final CANSparkMax m_followerMotorToTest = new CANSparkMax(0, MotorType.kBrushless);
+    private final CANSparkMax m_followerMotorToTest = new CANSparkMax(17, MotorType.kBrushless);
     
     
 
@@ -48,14 +49,14 @@ public class Mechanism extends SubsystemBase {
         m_motorToTest.restoreFactoryDefaults();
         m_motorToTest.setSmartCurrentLimit(60);
 
-        double gearRatio = 1;
+        double gearRatio = 80.0/1.0;
         // GEAR RATIO (convert from motor rotations to output rotations)
         m_motorToTest.getEncoder().setPositionConversionFactor(1/gearRatio);
 
         // GEAR RATIO DIVIDED BY 60 (convert from motor RPM to output RPS)
         m_motorToTest.getEncoder().setVelocityConversionFactor((1/gearRatio)*(1/60.0));
 
-        m_followerMotorToTest.follow(m_followerMotorToTest);
+        m_followerMotorToTest.follow(m_followerMotorToTest, true);
     }
 
     public Command joystickDriveCommand(DoubleSupplier output) {
@@ -66,5 +67,10 @@ public class Mechanism extends SubsystemBase {
     }
     public Command sysIdDynamic(SysIdRoutine.Direction direction) {
         return m_SysIdRoutine.dynamic(direction);
+    }
+
+    @Override
+    public void periodic() {
+        SmartDashboard.putNumber("Left Pivot Encoder", m_motorToTest.getEncoder().getPosition());
     }
 }
